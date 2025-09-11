8f2170965e6c0ba50e45fe7b93f61d768b46e210
Adalto Selau Sparremberger <adalto.sparremberger@animaeducacao.com.br>
2025-09-10 21:49:04 -0300

    Aula03 - JSON - servidor.php finalizado

--- Aula03-JSON/servidor.php
+++ Aula03-JSON/servidor.php
@@ -5,15 +5,45 @@
     $password = "" ;
     $banco = "loja_25_2";
 
-    $conn = mysqli_connect($local, $user, $password, $banco);
-    if( $conn ){
-        $query = "SELECT * FROM produto ORDER BY nome";
-        $result = mysqli_query( $conn , $query );
-        $linhas = array();
-        while( $row = mysqli_fetch_assoc($result)){
-            $linhas[] = $row;
+    if( isset($_REQUEST["buscar"]) ){
+        $conn = mysqli_connect($local, $user, $password, $banco);
+        if( $conn ){
+            $query = "SELECT * FROM produto ORDER BY nome";
+            $result = mysqli_query( $conn , $query );
+            $linhas = array();
+            while( $row = mysqli_fetch_assoc($result)){
+                $linhas[] = $row;
+            }
+            echo ' { "produtos" : '. json_encode($linhas).' } ';
+        }else{
+            echo ' { "erro" : "Erro ao tentar conectar no banco de dados"} ';
         }
-        echo ' { "produtos" : '. json_encode($linhas).' } ';
-    }else{
-        echo ' { "erro" : "Erro ao tentar conectar no banco de dados"} ';
+    }
+
+    if( isset($_REQUEST["inserir"]) ){
+        $conn = mysqli_connect($local, $user, $password, $banco);
+        if( $conn ){
+            $nome = $_POST["name"];
+            $preco = $_POST["price"];
+            $query = "INSERT INTO produto (nome, preco) VALUES ( '$nome' , $preco  )";
+            mysqli_query( $conn , $query );
+            $id = mysqli_insert_id($conn);
+            
+            echo ' { "resposta" : "Produto cadastrado!" , "idGerado" : '.$id.' } ';
+        }else{
+            echo ' { "resposta" : "Erro ao tentar conectar no banco de dados"} ';
+        }
+    }
+
+    if( isset($_REQUEST["excluir"]) ){
+        $conn = mysqli_connect($local, $user, $password, $banco);
+        if( $conn ){
+            $idProd = $_GET["idProduto"];
+            $query = "DELETE FROM produto WHERE id = $idProd";
+            mysqli_query( $conn , $query );
+            
+            echo ' { "resposta" : "Produto removido com sucessso!"  } ';
+        }else{
+            echo ' { "resposta" : "Erro ao tentar conectar no banco de dados"} ';
+        }
     }
