abdd8cd493a8872c6e5a60357ab1799964749da7
ADALTO SELAU SPARREMBERGER <adalto.sparremberger@ulife.com.br>
2025-11-12 21:53:32 -0300

    implementado o listar da integração

--- aula-integracao/script.js
+++ aula-integracao/script.js
@@ -23,3 +23,33 @@ function cadastrar(){
     }
    
 }
+
+
+function listar(){
+    const req = new XMLHttpRequest()
+    req.onreadystatechange = function(){
+        if( this.readyState == 4 && this.status == 200){
+            const tabela = document.getElementById("tblProdutos")
+            const produtos = JSON.parse( this.responseText )
+
+            produtos.forEach( prod => {
+                if( document.getElementById( "p" + prod.id ) == null ){
+                    var linha = tabela.insertRow( -1 )
+                    linha.id = "p" + prod.id
+                    cellId = linha.insertCell( 0 )
+                    cellNome = linha.insertCell( 1 )
+                    cellPreco = linha.insertCell( 2 )
+                    cellExcluir = linha.insertCell( 3 )
+
+                    cellId.innerHTML = prod.id
+                    cellNome.innerHTML = prod.nome
+                    cellPreco.innerHTML = prod.preco
+                    cellExcluir.innerHTML = '<button class="btn btn-danger" onclick="excluir('+prod.id+')"> X </button>'
+                }
+            })
+
+        }
+    }
+    req.open("GET" , "http://localhost:8001/product", true)
+    req.send()
+}
